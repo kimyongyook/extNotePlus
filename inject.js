cfb6649d4ad3bd25f92543b263e2629dcf225d67
kimyongyook <kimyongyook93@gmail.com>
2020-02-12 11:27:12 +0900

    [20.02.12] 1. js로드 시점 수정

--- inject.js
+++ inject.js
@@ -1,9 +1,12 @@
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-    var act = changes["act"].newValue;
-    console.log(act);
-    // console.log();
-    document.getElementById("draft-status-lozenge").setAttribute("original-title","초안아아아아");
-    document.getElementById("wow").appendChild("<span>test</span>");
-    // $("#draft-status-lozenge").setAttribute("original-title","초안아아아아");
-    // $("#draft-status-lozenge").append("<span>test</span>");
-});
+window.onload = function(){
+    chrome.storage.onChanged.addListener(function(changes, namespace) {
+    
+        var act = changes["act"].newValue;
+        console.log(act);
+        
+        document.getElementById("draft-status-lozenge").append("<span>test</span>");
+    
+    });
+}
+
+
